4b38f6304897d2d7058c18105db4049348310744
Ha <Vien.Ha@seamk.fi>
2025-03-13 11:13:10 +0200

    feat: using socket.io and google chart table

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-// const socket = require("socket.io");
+const socket = require("socket.io");
 
 app.use(express.json())
 
@@ -12,6 +12,8 @@ app.use(cors())
 app.set('view engine', 'ejs');
 
 // list for the measurements
+
+
 let measurementsArray = []
 
 app.get('/', (request, response) => {
@@ -24,26 +26,34 @@ app.get('/api/measurements', (request, response) => {
 app.post('/api/measurements', (request, response) => {
   const body = request.body
 
-  if(!body.field1){
+  if (!body.field1) {
     return response.status(400).json({ 
-        error: 'content missing' 
-      })
+      error: 'content missing' 
+    })
   }
+
   let id = measurementsArray.length + 1
-  const arrayrow = [id, body.field1, body.field2, body.field3]
+  const arrayrow = [id.toString(), body.field1, body.field2, body.field3]
 
   measurementsArray.push(arrayrow)
 
   // lähetetään lista listoja [[,,,], [,,,]...]
   var s = JSON.stringify(measurementsArray);
-
+  console.log(arrayrow);
   // send all measurements
-//   io.emit('measdata', s);
+  io.emit('measdata', s);
   console.log(arrayrow)
 
   response.json(arrayrow)
 })  
 
+
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const server = app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  console.log('New connection')
+})
